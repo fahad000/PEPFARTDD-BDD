3688647432aeb562fcee1746fe9cfe6dac79fe29
fahad <fkhan@usa-ctc.com>
2019-01-28 13:30:23 -0500

    Delete all the feature files and added one suite class

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.FactsInfo.PEPFAR.pages.stepdefs.MechanismsPage;
+import com.FactsInfo.PEPFARPage.LogingPage;
+import com.FactsInfo.PEPFARPage.MechanismSearchPage;
+
+@RunWith(Suite.class)
+@SuiteClasses({MechanismsPage.class,})
+public class AllTests {
+
+}
